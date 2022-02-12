3a53d53441d004bf6ccbfd2dd267a2bc2c19a0f2
Juana Patankar <juanapatankar4@gmail.com>
2022-02-12 03:55:24 +0000

    fixed neighbours

--- main.java
+++ main.java
@@ -1,10 +1,22 @@
 import java.util.Arrays;
 
 class Lab1 {
+    static int n = 10;
+    public static Node[] getNeighbours(Node node, Node[] ring) {
+        Node[] neighbours = new Node[n-1];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (ring[i] != node) {
+                neighbours[count] = ring[i];
+                count++;
+            }
+        }
+        return neighbours;
+    }
     public static void main(String[] args) {
         System.out.println("hi");
         Node[] ring = new Node[10];
-        int n = 10;
+        
         // assign id to every node in ring
         for (int id = 1; id <= n;id++) {
             Node currNode = new Node();
@@ -23,20 +35,14 @@ class Lab1 {
                 ring[id-1].next = ring[id];
             }
         }
-        // FIX THIS
-        /*int count = 0;
-        for (int id = 1; id <= n; id++) {
-            for (int nid = 1; nid <= n; nid++) {
-                if (nid != ring[id-1].id) {
-                
-                    ring[id-1].neighbours[count] = ring[nid-1];
-                }
-            }
-            
-        } */
-        //tests nodes are linked correctly
+        //tests nodes are linked correctly, and finding neighbours works
         for (int i = 0; i < 10; i++) {
-            System.out.println(String.format("Node: %s\nPrevious node: %s\nNext node: %s\nNeighbour IDs: %s\n",ring[i].id, ring[i].prev.id,ring[i].next.id, Arrays.toString(ring[i].neighbours)));
+            Node[] neighbours = getNeighbours(ring[i], ring);
+            int[] nids = new int[neighbours.length];
+            for (int n = 0; n < nids.length; n++) {
+                nids[n] = neighbours[n].id;
+            }
+            System.out.println(String.format("Node: %s\nPrevious node: %s\nNext node: %s\nNeighbour IDs: %s\n",ring[i].id, ring[i].prev.id,ring[i].next.id, Arrays.toString(nids)));
         }
     }
 } 
