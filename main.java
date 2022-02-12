9162e5d3790177e32a381b0e8bd1af6b047c30d2
Juana Patankar <juanapatankar4@gmail.com>
2022-02-12 04:21:24 +0000

    created initRing

--- main.java
+++ main.java
@@ -2,8 +2,11 @@ import java.util.Arrays;
 
 class Lab1 {
     static int n = 10;
+    static Node[] ring = new Node[n];
+    static int[] nids = new int[n-1];
     public static Node[] getNeighbours(Node node, Node[] ring) {
         Node[] neighbours = new Node[n-1];
+        neighbours = new Node[n-1];
         int count = 0;
         for (int i = 0; i < n; i++) {
             if (ring[i] != node) {
@@ -13,16 +16,15 @@ class Lab1 {
         }
         return neighbours;
     }
-    public static void main(String[] args) {
-        System.out.println("hi");
-        Node[] ring = new Node[10];
-        
-        // assign id to every node in ring
+
+    public static Node[] initRing() {
+        // create ring and assign id to every node in ring
         for (int id = 1; id <= n;id++) {
             Node currNode = new Node();
             currNode.id =id;
             ring[id-1] = currNode;
         } 
+        // link nodes to prev and next
         for (int id = 1; id <= n; id++) {
             if (id == 1) {
                 ring[id-1].prev = ring[n-1];
@@ -35,14 +37,25 @@ class Lab1 {
                 ring[id-1].next = ring[id];
             }
         }
+        for (int i = 0; i < n; i++) {
+            ring[i].neighbours = getNeighbours(ring[i], ring);
+            for (int n = 0; n < nids.length; n++) {
+                nids[n] = ring[i].neighbours[n].id;
+            }
+            System.out.println(String.format("Node: %s\nPrevious node: %s\nNext node: %s\nNeighbour IDs: %s\n",ring[i].id, ring[i].prev.id,ring[i].next.id, Arrays.toString(nids)));
+        }
+        return ring;
+    }
+    public static void main(String[] args) {
+        ring = initRing();
         //tests nodes are linked correctly, and finding neighbours works
-        for (int i = 0; i < 10; i++) {
-            Node[] neighbours = getNeighbours(ring[i], ring);
+        /* for (int i = 0; i < n; i++) {
+            neighbours = getNeighbours(ring[i], ring);
             int[] nids = new int[neighbours.length];
             for (int n = 0; n < nids.length; n++) {
                 nids[n] = neighbours[n].id;
             }
             System.out.println(String.format("Node: %s\nPrevious node: %s\nNext node: %s\nNeighbour IDs: %s\n",ring[i].id, ring[i].prev.id,ring[i].next.id, Arrays.toString(nids)));
-        }
+        } */
     }
 } 
